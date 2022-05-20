1612ece8650a97c00963cda06a05c95d826895c9
bryce <1907004005@gmail.com>
2022-05-21 00:09:59 +0800

    feat: fix lint error

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from 'tsup'
 export const tsup: Options = {
   entry: [
     './src/index.ts',
-    './src/gsap/index.ts'
+    './src/gsap/index.ts',
   ],
   external: [
     'vue',
